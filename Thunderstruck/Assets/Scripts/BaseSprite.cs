6f4ff99223bc613107b8e7466d6110e8c8e77746
Connor Goldsmith <cgolds2@lsu.edu>
2019-02-26 18:10:04 -0600

    Basic Player Movement around map

---
+++ Thunderstruck/Assets/Scripts/BaseSprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSprite : MonoBehaviour {
+
+	// Use this for initialization
+	public void BaseStart () {
+		
+	}
+	
+	// Update is called once per frame
+	public void BaseUpdate() {
+        
+        if (transform.position.x <= -6.75f)
+        {
+            transform.position = new Vector2(-6.75f, transform.position.y);
+        }
+        else if (transform.position.x >= 6.75f)
+        {
+            transform.position = new Vector2(6.75f, transform.position.y);
+        }
+
+        // Y axis
+        if (transform.position.y <= -3.05f)
+        {
+            transform.position = new Vector2(transform.position.x, -3.05f);
+        }
+        else if (transform.position.y >= 3.05f)
+        {
+            transform.position = new Vector2(transform.position.x, 3.05f);
+        }
+    }
+}
---
+++ Thunderstruck/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : BaseSprite {
+    public float panSpeed;
+    // Use this for initialization
+    void Start () {
+        panSpeed = 10;
+	}
+    
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+        base.BaseUpdate();
+
+    }
+
+}
