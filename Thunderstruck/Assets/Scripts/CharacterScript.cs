561360832a3b39c7ddbb1e0ae12612f9f9894d68
Connor Goldsmith <cgolds2@lsu.edu>
2019-02-26 18:28:00 -0600

    Enemy can call player

--- Thunderstruck/Assets/Scripts/CharacterScript.cs
+++ Thunderstruck/Assets/Scripts/CharacterScript.cs
@@ -4,35 +4,52 @@ using UnityEngine;
 
 public class CharacterScript : BaseSprite {
     public float panSpeed;
+    public float health;
+    public int iFrames;
     // Use this for initialization
     void Start () {
         panSpeed = 10;
+        health = 5;
+        iFrames = 0;
 	}
     
-
+    public bool IsAlive()
+    {
+        return health > 0;
+    }
     // Update is called once per frame
     void Update () {
-        Vector3 pos = transform.position;
+        if (IsAlive())
+        {
+            Vector3 pos = transform.position;
 
-        if (Input.GetKey("w"))
-        {
-            pos.y += panSpeed * Time.deltaTime;
+            if (Input.GetKey("w"))
+            {
+                pos.y += panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("s"))
+            {
+                pos.y -= panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("d"))
+            {
+                pos.x += panSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("a"))
+            {
+                pos.x -= panSpeed * Time.deltaTime;
+            }
+            transform.position = pos;
         }
-        if (Input.GetKey("s"))
-        {
-            pos.y -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-        }
-        transform.position = pos;
         base.BaseUpdate();
 
-    }
 
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            health--;
+        }
+    }
 }
