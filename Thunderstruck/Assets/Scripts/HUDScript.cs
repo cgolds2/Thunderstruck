57faa5025ff01bfec39a95c2ee3ca7c5169af644
Connor Goldsmith <cgolds2@lsu.edu>
2019-03-14 11:31:17 -0500

    Small commit for hud script

---
+++ Thunderstruck/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour
+{
+    public static GameObject hud;
+    public static GameObject score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud = GameObject.Find("HUD");
+        score = GameObject.Find("Score");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
