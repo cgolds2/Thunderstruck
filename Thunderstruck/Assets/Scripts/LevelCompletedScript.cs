91a444a2669279300598593e6586e9c11962c0cc
Connor Goldsmith <cgolds2@lsu.edu>
2019-03-20 17:36:06 -0500

    Made demo stop after level 2

---
+++ Thunderstruck/Assets/Scripts/LevelCompletedScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompletedScript : MonoBehaviour
+{
+    public Text LevelCompleted;
+    public GameObject NextLevelButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (HUDScript.GetLevel() >= 2)
+        {
+            NextLevelButton.SetActive(false);
+            LevelCompleted.text = "Demo Completed";
+
+        }
+        else
+        {
+            LevelCompleted.text = string.Format("Level {0} Completed", HUDScript.GetLevel());
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
